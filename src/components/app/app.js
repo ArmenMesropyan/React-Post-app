ebb32af1c0c1e837269daeca13ecb2f097643a35
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-13 16:58:25 +0400

    Search panel html added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
 
 const App = () => (
     <main className="posts">
         <h1 className="visually-hidden">Post application - a simple react application.</h1>
         <AppHeader />
+        <section className="search-panel">
+            <SearchPanel />
+        </section>
     </main>
 );
 
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchPanel = () => {
+    return (
+        <div className="container">
+            <input 
+                className="search-panel__input"
+                type="text"
+                placeholder="Search by records"
+            />
+        </div>
+    )
+};
+
+export default SearchPanel;
