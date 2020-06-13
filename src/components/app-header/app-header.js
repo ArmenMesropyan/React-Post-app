c3ed10f7bac893fd352278d4d0299f13de2dbaf5
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-13 16:44:05 +0400

    App header main styles added

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,14 +3,17 @@ import React from "react";
 const AppHeader = () => {
     return (
         <header className="posts__header posts-header">
-            <ul className="posts-header__list">
-                <li className="posts-header__item">
-                    <h2 className="posts-header__name">Armen Mesropyan</h2>
-                </li>
-                <li className="posts-header__item">
-                    <h2 className="posts-header__stats">5 entries, 0 of them liked</h2>
-                </li>
-            </ul>
+            <h2 className="visually-hidden">User information</h2>
+            <div className="container">
+                <ul className="posts-header__list">
+                    <li className="posts-header__item">
+                        <h3 className="posts-header__name">Armen Mesropyan</h3>
+                    </li>
+                    <li className="posts-header__item">
+                        <p className="posts-header__stats">5 entries, 0 of them liked</p>
+                    </li>
+                </ul>
+            </div>
         </header>
     )
 };
