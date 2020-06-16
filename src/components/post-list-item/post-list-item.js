13bb465084c15bbba89f91767a1500c5b0dcbf6d
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-16 17:24:23 +0400

    Star click event added, state changed

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PostList = ({label, important}) => {
-    const classNames = `posts-list__item ${important ? 'posts-list__item_important': ''}`;
-    return (
-        <li className={classNames}>
-            <h3 className="posts-list__heading">{label}</h3>
-            <ul className="posts-list__actions actions-list">
-                <li className="actions-list__item actions-list__item_star">
-                    <button type="button" className="actions-list__btn">
-                        <i className="fa fa-star"></i>
-                    </button>
-                </li>
-                <li className="actions-list__item actions-list__item_trash">
-                    <button type="button" className="actions-list__btn">
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </li>
-                <li className="actions-list__item actions-list__item_heart">
-                    <button type="button" className="actions-list__btn">
-                        <i className="fa fa-heart"></i>
-                    </button>
-                </li>
-            </ul>
-        </li>
-    );
+export default class PostListItem extends Component {
+    state = {
+        important: this.props.important || false,
+    }
+
+    onImportantBtnClick = () => {
+        this.setState(({important}) => ({important: !important}));
+    }
+
+    render() {
+        const {label} = this.props;
+        const {important} = this.state;
+
+        const classNames = `posts-list__item ${important ? 'posts-list__item_important': ''}`;
+
+        return (
+            <li className={classNames}>
+                <h3 className="posts-list__heading">{label}</h3>
+                <ul className="posts-list__actions actions-list">
+                    <li className="actions-list__item actions-list__item_star">
+                        <button type="button" className="actions-list__btn" onClick={this.onImportantBtnClick}>
+                            <i className="fa fa-star"></i>
+                        </button>
+                    </li>
+                    <li className="actions-list__item actions-list__item_trash">
+                        <button type="button" className="actions-list__btn">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </li>
+                    <li className="actions-list__item actions-list__item_heart">
+                        <button type="button" className="actions-list__btn">
+                            <i className="fa fa-heart"></i>
+                        </button>
+                    </li>
+                </ul>
+            </li>
+        );
+    }
 }
-
-export default PostList;
