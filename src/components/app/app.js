b8aa6395abf5bb51d2665b0526015d8ad4c1117b
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-16 16:35:05 +0400

    Simple data array for test added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,19 +8,28 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
-const App = () => (
-    <main className="posts">
-        <h1 className="visually-hidden">Post application - a simple react application.</h1>
-        <AppHeader />
-        <section className="search-panel">
-            <div className="container search-panel__wrapper">
-                <SearchPanel />
-                <PostStatusFilter />
-            </div>
-        </section>
-        <PostList />
-        <PostAddForm />
-    </main>
-);
+const App = () => {
+    const posts = [
+        {label: 'I want to learn React!', important: true, id: 'qsdw12kl01123kasd'},
+        {label: 'Hi everyone!', important: false, id: 'kla12ds92jnzio'},
+        {label: 'I will create new application!', important: false, id: 'lmzx014nxz0p'},
+        {label: 'Whats wrong with world?!', important: false, id: 'polaz123zx0l'},
+    ]
+    
+    return (
+        <main className="posts">
+            <h1 className="visually-hidden">Post application - a simple react application.</h1>
+            <AppHeader />
+            <section className="search-panel">
+                <div className="container search-panel__wrapper">
+                    <SearchPanel />
+                    <PostStatusFilter />
+                </div>
+            </section>
+            <PostList />
+            <PostAddForm />
+        </main>
+    )
+};
 
 export default App;
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -4,7 +4,6 @@ const PostList = () => {
     return (
         <li className="posts-list__item">
             <h3 className="posts-list__heading">Hello World!</h3>
-            {/* <i className="fa fa-heart"></i> */}
             <ul className="posts-list__actions actions-list">
                 <li className="actions-list__item actions-list__item_star">
                     <button type="button" className="actions-list__btn">
