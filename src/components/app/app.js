e3dd6c8d162c8da83369ec7bf827f84fe8515081
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-17 20:05:04 +0400

    App component changed to class and state changed, when post deleted

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -6,32 +6,38 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
-const App = () => {
-    const posts = [
-        {label: 'I want to learn React!', important: true, liked: true, id: 'qsdw12kl01123kasd'},
-        {label: 'Hi everyone!', important: false, id: 'kla12ds92jnzio'},
-        {label: 'I will create new application!', important: false, id: 'lmzx014nxz0p'},
-        {label: 'Whats wrong with world?!', important: false, id: 'polaz123zx0l'},
-    ]
+export default class App extends Component {
 
-    function deletePost(id) {
-        console.log(`delete ${id}`);
+    state = {
+        posts: [
+            {label: 'I want to learn React!', important: true, liked: true, id: 'qsdw12kl01123kasd'},
+            {label: 'Hi everyone!', important: false, id: 'kla12ds92jnzio'},
+            {label: 'I will create new application!', important: false, id: 'lmzx014nxz0p'},
+            {label: 'Whats wrong with world?!', important: false, id: 'polaz123zx0l'},
+        ]
     }
-    
-    return (
-        <main className="posts">
-            <h1>Post application - a simple react application.</h1>
-            <AppHeader />
-            <section className="search-panel">
-                <div className="container search-panel__wrapper">
-                    <SearchPanel />
-                    <PostStatusFilter />
-                </div>
-            </section>
-            <PostList posts={posts} onDelete={deletePost}/>
-            <PostAddForm />
-        </main>
-    )
-};
 
-export default App;
+    deletePost = (id) => {
+        this.setState(({posts}) => ({
+            posts: posts.filter(post => post.id !== id),
+        }))
+    }
+
+    render() {
+        const {posts} = this.state;
+        return (
+            <main className="posts">
+                <h1>Post application - a simple react application.</h1>
+                <AppHeader />
+                <section className="search-panel">
+                    <div className="container search-panel__wrapper">
+                        <SearchPanel />
+                        <PostStatusFilter />
+                    </div>
+                </section>
+                <PostList posts={posts} onDelete={this.deletePost}/>
+                <PostAddForm />
+            </main>
+        );
+    }
+};
